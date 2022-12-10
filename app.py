2d1717bb0fc15d2c0134f430220e70a69c75f4c0
tacochan0129 <119779506+tacochan0129@users.noreply.github.com>
2022-12-10 14:07:35 +0800

    回歸

--- app.py
+++ app.py
@@ -48,65 +48,16 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = text=event.message.text
-    if re.match('有不錯的咖啡廳嗎',message):
-        carousel_template_message = TemplateSendMessage(
-             alt_text='免費教學影片',
-             template=CarouselTemplate(
-                 columns=[
-                     CarouselColumn(
-                         thumbnail_image_url='https://i.imgur.com/wpM584d.jpg',
-                         title='Python基礎教學',
-                         text='萬丈高樓平地起',
-                         actions=[
-                             MessageAction(
-                                 label='教學內容',
-                                 text='拆解步驟詳細介紹安裝並使用Anaconda、Python、Spyder、VScode…'
-                             ),
-                             URIAction(
-                                 label='馬上查看',
-                                 uri='https://marketingliveincode.com/?page_id=270'
-                             )
-                         ]
-                     ),
-                     CarouselColumn(
-                         thumbnail_image_url='https://i.imgur.com/W7nI6fg.jpg',
-                         title='Line Bot聊天機器人',
-                         text='台灣最廣泛使用的通訊軟體',
-                         actions=[
-                             MessageAction(
-                                 label='教學內容',
-                                 text='Line Bot申請與串接'
-                             ),
-                             URIAction(
-                                 label='馬上查看',
-                                 uri='https://marketingliveincode.com/?page_id=2532'
-                             )
-                         ]
-                     ),
-                     CarouselColumn(
-                         thumbnail_image_url='https://i.imgur.com/l7rzfIK.jpg',
-                         title='Telegram Bot聊天機器人',
-                         text='唯有真正的方便，能帶來意想不到的價值',
-                         actions=[
-                             MessageAction(
-                                 label='教學內容',
-                                 text='Telegrame申請與串接'
-                             ),
-                             URIAction(
-                                 label='馬上查看',
-                                 uri='https://marketingliveincode.com/?page_id=2648'
-                             )
-                         ]
-                     )
-                 ]
-             )
-         )
-         line_bot_api.reply_message(event.reply_token, carousel_template_message)
-     else:
-         line_bot_api.reply_message(event.reply_token, TextSendMessage("收到囉!"))
+    if re.match('告訴我秘密',message):
+        image_message = ImageSendMessage(
+            original_content_url='https://maruko.tw/wp-content/uploads/flickr/48727790946_2f625f443d_b.jpg',
+            preview_image_url='https://wowlavie-aws.hmgcdn.com/file/article_all/%E5%A4%A7%E7%A8%BB%E5%9F%95%E3%80%8CTWATUTIA%E3%80%8D%E5%92%96%E5%95%A1%E5%BB%B31.jpg'
+        )
+        line_bot_api.reply_message(event.reply_token, image_message)
+    else:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage("收到囉!"))
 #主程式
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-
