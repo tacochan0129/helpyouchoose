41ce85a2d845bbd2db61f0e1c593d4d29703d4f1
tacochan0129 <119779506+tacochan0129@users.noreply.github.com>
2022-12-10 14:48:57 +0800

    按鈕樣板

--- app.py
+++ app.py
@@ -49,13 +49,32 @@ def callback():
 def handle_message(event):
     message = text=event.message.text
     if re.match('告訴我秘密',message):
-        image_message = ImageSendMessage(
-            original_content_url='https://maruko.tw/wp-content/uploads/flickr/48727790946_2f625f443d_b.jpg',
-            preview_image_url='https://wowlavie-aws.hmgcdn.com/file/article_all/%E5%A4%A7%E7%A8%BB%E5%9F%95%E3%80%8CTWATUTIA%E3%80%8D%E5%92%96%E5%95%A1%E5%BB%B31.jpg'
+        buttons_template_message = TemplateSendMessage(
+        alt_text='這個看不到',
+        template=ButtonsTemplate(
+            thumbnail_image_url='https://i.imgur.com/wpM584d.jpg',
+            title='行銷搬進大程式',
+            text='選單功能－TemplateSendMessage',
+            actions=[
+                PostbackAction(
+                    label='偷偷傳資料',
+                    display_text='檯面上',
+                    data='action=檯面下'
+                ),
+                MessageAction(
+                    label='光明正大傳資料',
+                    text='我就是資料'
+                ),
+                URIAction(
+                    label='行銷搬進大程式',
+                    uri='https://marketingliveincode.com/'
+                )
+            ]
         )
-        line_bot_api.reply_message(event.reply_token, image_message)
+    )
+        line_bot_api.reply_message(event.reply_token, buttons_template_message)
     else:
-        line_bot_api.reply_message(event.reply_token, TextSendMessage("收到囉!"))
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
 #主程式
 import os
 if __name__ == "__main__":
