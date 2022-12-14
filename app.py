3cc271d996617aaf8350330be5cd4ad73053d68b
tacochan0129 <119779506+tacochan0129@users.noreply.github.com>
2022-12-15 00:38:06 +0800

    Debug try19
    隨便試試啦

--- app.py
+++ app.py
@@ -25,7 +25,23 @@ cf = open("cafe.csv", "r", encoding="utf-8")
 csv_reader = csv.DictReader(cf)
 cf_row = [row for row in csv_reader]
 cf.close()
-
+#隨機抽取三家店
+cf_random = sample(cf_row,3)
+cafe1 = cf_random[0]
+cafe2 = cf_random[1]
+cafe3 = cf_random[2]
+ #將名稱、敘述、GoogleMaps連結、圖片存進functions
+def name(cafe_num) :
+   return cafe_num['咖啡廳名稱']
+def text(cafe_num):
+    return cafe_num['敘述']
+def gmap(cafe_num):
+    return cafe_num['GoogleMaps']
+def pic(cafe_num):
+    return cafe_num['圖片1']
+def thumb(cafe_num):
+    return cafe_num['封面']
+        
 # 放上自己的Channel Access Token
 line_bot_api = LineBotApi('YeDTarsdKiytdqoOC7qQIl/JjhRCNK3UTSj5rUT4vguYoCgASBdMutqc/2yQUdgWf68PJSrqegY9JRm9p97kKu0e3M3BgyTqiWBFdnY5Ugl0huQrHvUbGRqUKa/xhJAJjTMO3rD/rYOcbl5IyKunvAdB04t89/1O/w1cDnyilFU=')
 # 放上自己的Channel Secret
@@ -56,65 +72,56 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = text=event.message.text
+        
     if re.match('咖啡廳輪盤',message):
+        
         #隨機抽取三家店
         cf_random = sample(cf_row,3)
         cafe1 = cf_random[0]
         cafe2 = cf_random[1]
         cafe3 = cf_random[2]
         
-       #將名稱、敘述、GoogleMaps連結、圖片存進變數
-        name1 = cafe1['咖啡廳名稱']
-        name2 = cafe2['咖啡廳名稱']
-        name3 = cafe3['咖啡廳名稱']
+#        #將名稱、敘述、GoogleMaps連結、圖片存進變數
+#         name1 = cafe1['咖啡廳名稱']
+#         name2 = cafe2['咖啡廳名稱']
+#         name3 = cafe3['咖啡廳名稱']
         
-        text1 = cafe1['敘述']
-        text2 = cafe2['敘述']
-        text3 = cafe3['敘述']
+#         text1 = cafe1['敘述']
+#         text2 = cafe2['敘述']
+#         text3 = cafe3['敘述']
         
-        map1 = cafe1['GoogleMaps']
-        map2 = cafe2['GoogleMaps']
-        map3 = cafe3['GoogleMaps']
+#         map1 = cafe1['GoogleMaps']
+#         map2 = cafe2['GoogleMaps']
+#         map3 = cafe3['GoogleMaps']
         
-        pic1 = cafe1['圖片1']
-        pic2 = cafe2['圖片1']
-        pic3 = cafe3['圖片1']
+#         pic1 = cafe1['圖片1']
+#         pic2 = cafe2['圖片1']
+#         pic3 = cafe3['圖片1']
         
-        thumb1 = cafe1['封面']
-        thumb2 = cafe2['封面']
-        thumb3 = cafe3['封面']
-        
-#        #將名稱、敘述、GoogleMaps連結、圖片存進functions
-#         def name(cafe_num) :
-#             return cafe_num['咖啡廳名稱']
-#         def text(cafe_num):
-#             return cafe_num['敘述']
-#         def gmap(cafe_num):
-#             return cafe_num['GoogleMaps']
-#         def pic(cafe_num):
-#             return cafe_num['圖片1']
-#         def thumb(cafe_num):
-#             return cafe_num['封面']
+#         thumb1 = cafe1['封面']
+#         thumb2 = cafe2['封面']
+#         thumb3 = cafe3['封面']
+
         image_carousel_template_message = TemplateSendMessage(
             alt_text='咖啡廳輪盤',
             template=ImageCarouselTemplate(
                 columns=[
                     ImageCarouselColumn(
-                        image_url= pic1,#cafe1['圖片1']
+                        image_url= pic(cafe1),#cafe1['圖片1']
                         action=MessageAction(
                             label='選這個！',
                             text='我選第一張'
                         )
                     ),
                     ImageCarouselColumn(
-                        image_url= pic2,#cafe2['圖片1']
+                        image_url= pic(cafe2),#cafe2['圖片1']
                         action=MessageAction(
                             label='選這個！',
                             text='我選第二張'
                         )
                     ),
                     ImageCarouselColumn(
-                        image_url= pic3,#cafe3['圖片1']
+                        image_url= pic(cafe3),#cafe3['圖片1']
                         action=MessageAction(
                             label='選這個！',
                             text='我選第三張'
@@ -129,13 +136,13 @@ def handle_message(event):
         buttons_template_message = TemplateSendMessage(
         alt_text='就決定是你了！',
         template=ButtonsTemplate(
-            thumbnail_image_url=thumb1,#cafe1['封面']
-            title=name1,#cafe1['咖啡廳名稱']
-            text=text1,#cafe1['敘述']
+            thumbnail_image_url=thumb(cafe1),#cafe1['封面']
+            title=name(cafe1),#cafe1['咖啡廳名稱']
+            text=text(cafe1),#cafe1['敘述']
             actions=[
                 URIAction(
                     label='現在就過去吧！',
-                    uri=map1#cafe1['GoogleMaps']
+                    uri=gmap(cafe1)#cafe1['GoogleMaps']
                 )
             ]
         )
@@ -146,13 +153,13 @@ def handle_message(event):
         buttons_template_message = TemplateSendMessage(
         alt_text='就決定是你了！',
         template=ButtonsTemplate(
-            thumbnail_image_url=thumb2,#cafe2['封面']
-            title=name2,#cafe2['咖啡廳名稱']
-            text=text2,#cafe2['敘述']
+            thumbnail_image_url=thumb(cafe2),#cafe2['封面']
+            title=name(cafe2),#cafe2['咖啡廳名稱']
+            text=text(cafe2),#cafe2['敘述']
             actions=[
                 URIAction(
                     label='現在就過去吧！',
-                    uri=map2#cafe2['GoogleMaps']
+                    uri=gmap(cafe2)#cafe2['GoogleMaps']
                 )
             ]
         )
@@ -163,13 +170,13 @@ def handle_message(event):
         buttons_template_message = TemplateSendMessage(
         alt_text='就決定是你了！',
         template=ButtonsTemplate(
-            thumbnail_image_url=thumb3,#cafe3['封面']
-            title=name3,#cafe3['咖啡廳名稱']
-            text=text3,#cafe3['敘述']
+            thumbnail_image_url=thumb(cafe3),#cafe3['封面']
+            title=name(cafe3),#cafe3['咖啡廳名稱']
+            text=text(cafe3),#cafe3['敘述']
             actions=[
                 URIAction(
                     label='現在就過去吧！',
-                    uri=map3#cafe3['GoogleMaps']
+                    uri=gmap(cafe3)#cafe3['GoogleMaps']
                 )
             ]
         )
