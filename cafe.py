d966dc2fcbbb57954158586e8c8ad21a5f4ee0c2
tacochan0129 <119779506+tacochan0129@users.noreply.github.com>
2022-12-14 21:40:31 +0800

    Add files via upload

---
+++ cafe.py
@@ -0,0 +1,29 @@
+import csv
+from random import sample
+cf = open("cafe.csv", "r", encoding="utf-8")
+csv_reader = csv.DictReader(cf)
+cf_row = [row for row in csv_reader]
+cf.close()
+
+#隨機抽取三家店
+cf_random = sample(cf_row,3)
+cafe1 = cf_random[0]
+cafe2 = cf_random[1]
+cafe3 = cf_random[2]
+
+#將名稱、敘述、GoogleMaps連結、圖片存進functions
+def name(cafe_num) :
+    return cafe_num['咖啡廳名稱']
+
+def text(cafe_num):
+    return cafe_num['敘述']
+
+def map(cafe_num):
+    return cafe_num['GoogleMaps']
+
+def pic(cafe_num):
+    return cafe_num['圖片1']
+
+#print(cf_row)
+#print(cafe1)
+#print(name(cafe1), text(cafe1), map(cafe1), pic(cafe1))
