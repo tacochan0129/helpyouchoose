476dd03641521c54baccddbd8044ab7a7025ab01
tacochan0129 <119779506+tacochan0129@users.noreply.github.com>
2022-12-14 22:38:02 +0800

    Debug try6
    將原本範例檔案叫回來試試看

--- app.py
+++ app.py
@@ -108,23 +108,47 @@ def handle_message(event):
         )
         line_bot_api.reply_message(event.reply_token, image_carousel_template_message)
     
-   
     elif re.match('我選第一張',message):
         buttons_template_message = TemplateSendMessage(
-        alt_text='就決定是你了！',
+        alt_text='這個看不到',
         template=ButtonsTemplate(
-            thumbnail_image_url= thumb(cafe1),
-            title= name(cafe1),
-            text=　text1(cafe1),
+            thumbnail_image_url='https://i.imgur.com/wpM584d.jpg',
+            title='行銷搬進大程式',
+            text='選單功能－TemplateSendMessage',
             actions=[
+                PostbackAction(
+                    label='偷偷傳資料',
+                    display_text='檯面上',
+                    data='action=檯面下'
+                ),
+                MessageAction(
+                    label='光明正大傳資料',
+                    text='我就是資料'
+                ),
                 URIAction(
-                    label='現在就帶我過去',
-                    uri= gmap(cafe1)
+                    label='行銷搬進大程式',
+                    uri='https://marketingliveincode.com/'
                 )
             ]
         )
     )
         line_bot_api.reply_message(event.reply_token, buttons_template_message)
+#     elif re.match('我選第一張',message):
+#         buttons_template_message = TemplateSendMessage(
+#         alt_text='就決定是你了！',
+#         template=ButtonsTemplate(
+#             thumbnail_image_url= thumb(cafe1),
+#             title= name(cafe1),
+#             text=　text1(cafe1),
+#             actions=[
+#                 URIAction(
+#                     label='現在就帶我過去',
+#                     uri= gmap(cafe1)
+#                 )
+#             ]
+#         )
+#     )
+#         line_bot_api.reply_message(event.reply_token, buttons_template_message)
    
         
 #     elif re.match('我選第二張',message):
