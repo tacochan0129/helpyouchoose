419a5bdb710e5f12aa548c7815fd4c2b8055ea05
tacochan0129 <119779506+tacochan0129@users.noreply.github.com>
2022-12-17 16:50:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -70,6 +70,13 @@ def handle_message(event):
         return cafe_num['圖片1']
     def thumb(cafe_num):
         return cafe_num['封面']
+    
+    #隨機抽取三家店
+    cf_random = sample(cf_row,3)
+    cafe1 = cf_random[0]
+    cafe2 = cf_random[1]
+    cafe3 = cf_random[2]   
+
 #        #將名稱、敘述、GoogleMaps連結、圖片存進變數
 #         name1 = cafe1['咖啡廳名稱']
 #         name2 = cafe2['咖啡廳名稱']
@@ -91,11 +98,6 @@ def handle_message(event):
 #         thumb2 = cafe2['封面']
 #         thumb3 = cafe3['封面']
     if re.match('咖啡廳輪盤',message):
-        #隨機抽取三家店
-        cf_random = sample(cf_row,3)
-        cafe1 = cf_random[0]
-        cafe2 = cf_random[1]
-        cafe3 = cf_random[2]   
 
         image_carousel_template_message = TemplateSendMessage(
             alt_text='咖啡廳輪盤',
@@ -131,13 +133,13 @@ def handle_message(event):
         buttons_template_message = TemplateSendMessage(
         alt_text='就決定是你了！',
         template=ButtonsTemplate(
-            thumbnail_image_url="https://lh5.googleusercontent.com/p/AF1QipMYym6i-Su5vMllJWg0WseqO9uq92erISCCdFNq=w203-h203-k-no",#cafe1['封面']
-            title="1",#cafe1['咖啡廳名稱']
-            text="2",#cafe1['敘述']
+            thumbnail_image_url=thumb(cafe1),#cafe1['封面']
+            title=name(cafe1),#cafe1['咖啡廳名稱']
+            text=text(cafe1),#cafe1['敘述']
             actions=[
                 URIAction(
                     label='現在就過去吧！',
-                    uri="https://www.google.com.tw/maps/place/Weiyang+Coffee/@25.0195055,121.5475149,15z/data=!3m1!5s0x3442aa2dec92b191:0x43b8be25eb0b94c4!4m5!3m4!1s0x3442aa2dec922001:0xf1592a50aa99e9f9!8m2!3d25.0262375!4d121.5474209"#cafe1['GoogleMaps']
+                    uri=gmap(cafe1)#cafe1['GoogleMaps']
                 )
             ]
         )
