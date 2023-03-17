1fa81e267a4b34b537df3bcd6208d6dcf39f272e
Lucas20030209 <96294647+Lucas20030209@users.noreply.github.com>
2023-03-17 17:14:27 +0800

    debug 16!
    change to buttons template message!

--- app.py
+++ app.py
@@ -172,13 +172,13 @@ def handle_message(event):
             buttons_template_message = TemplateSendMessage(
             alt_text='就決定是你了！',
             template=ButtonsTemplate(
-                thumbnail_image_url=thumb_list[2],#cafe3['封面']
-                title=name_list[2],#cafe3['咖啡廳名稱']
-                text=text_list[2],#cafe3['敘述']
+                thumbnail_image_url=thumb_list[2], #cafe3['封面']
+                title=name_list[2], #cafe3['咖啡廳名稱']
+                text=text_list[2], #cafe3['敘述']
                 actions=[
                     URIAction(
                         label='現在就過去吧！',
-                        uri=map_list[2]#cafe3['GoogleMaps']
+                        uri=map_list[2] #cafe3['GoogleMaps']
                     )
                 ]
             )
@@ -233,30 +233,30 @@ def handle_message(event):
 #             maps_url = f'https://www.google.com/maps/search/?api=1&query={lat},{lng}&query_place_id={nearest_coffee_shop["place_id"]}'
         coffee_shop = nearest_coffee("新北市新莊區民樂街39號")
         coffee_name = coffee_shop[0]
-        coffee_rating = coffee_shop[1]
+        coffee_rating = str(coffee_shop[1])
         maps_url = coffee_shop[2]
-        thumbnail_image_url = coffee_shop[3]
+        thumbnail_url = coffee_shop[3]
 #         coffee_name = "Cafefe Libero"
 #         coffee_rating = "4.2"
 #         thumbnail_image_url = "https://play-lh.googleusercontent.com/Kf8WTct65hFJxBUDm5E-EpYsiDoLQiGGbnuyP6HBNax43YShXti9THPon1YKB6zPYpA"
 #         maps_url = "https://www.google.com.tw/maps/place/SECOND+FLOOR+CAFE+%E8%B2%B3%E6%A8%93%E4%BB%81%E6%84%9B/@25.0379115,121.5236378,15z/data=!3m1!5s0x3442a97ebf47ca7b:0x6fe70de6eeb4a6e4!4m6!3m5!1s0x3442a97ebf69e67b:0xf06276ea3de8b70!8m2!3d25.0379126!4d121.5323917!16s%2Fg%2F12hk8x73m"
-#         buttons_template_message = TemplateSendMessage(
-#         alt_text = '附近店家',
-#         template=ButtonsTemplate(
-#             thumbnail_image_url = thumbnail_image_url,
-#             title = coffee_name,
-#             text = "評分：" + coffee_rating,
-#             actions = [
-#                 URIAction(
-#                     label = '現在就過去吧！',
-#                     uri = maps_url
-#                 )
-#             ]
-#         )
-#     )
-#         line_bot_api.reply_message(event.reply_token, buttons_template_message)
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(coffee_name))
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(maps_url))
+        buttons_template_message = TemplateSendMessage(
+        alt_text = '附近店家',
+        template=ButtonsTemplate(
+            thumbnail_image_url = thumbnail_url,
+            title = coffee_name,
+            text = "評分：" + str(coffee_rating),
+            actions = [
+                URIAction(
+                    label = '現在就過去吧！',
+                    uri = maps_url
+                )
+            ]
+        )
+    )
+        line_bot_api.reply_message(event.reply_token, buttons_template_message)
+#         line_bot_api.reply_message(event.reply_token, TextSendMessage(coffee_name))
+#         line_bot_api.reply_message(event.reply_token, TextSendMessage(maps_url))
     else:
         line_bot_api.reply_message(event.reply_token, TextSendMessage('感謝您的使用❤️'))
 
