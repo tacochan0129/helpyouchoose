b7998fbf6025d53b910a5950b81674e54ed0df4a
Lucas20030209 <96294647+Lucas20030209@users.noreply.github.com>
2023-03-17 16:59:47 +0800

    debug 15?
    Big update! Add haversine function to get the "nearest" coffee shop!

--- app.py
+++ app.py
@@ -31,6 +31,7 @@ thumb_list = []
 
 # 附近店家功能
 from gps import *
+import rungps
 
 # 放上自己的Channel Access Token
 line_bot_api = LineBotApi('YeDTarsdKiytdqoOC7qQIl/JjhRCNK3UTSj5rUT4vguYoCgASBdMutqc/2yQUdgWf68PJSrqegY9JRm9p97kKu0e3M3BgyTqiWBFdnY5Ugl0huQrHvUbGRqUKa/xhJAJjTMO3rD/rYOcbl5IyKunvAdB04t89/1O/w1cDnyilFU=')
@@ -212,24 +213,29 @@ def handle_message(event):
 #     if event.message.type == 'location':
 #         address = event.message.address
 
-        near_coffee_shop_location = "106台北市大安區永康街37巷28號"
-        near_coffee_shop_dic = get_nearest_coffee_shop(near_coffee_shop_location)
-        lat = near_coffee_shop_dic['lat']
-        lng = near_coffee_shop_dic['lng']        
-        # set
-        radius = 100
-        keyword = 'coffee'
-        nearby_coffee = get_nearby_places(lat, lng, radius, keyword)
+#         near_coffee_shop_location = 
+#         near_coffee_shop_dic = get_nearest_coffee_shop(near_coffee_shop_location)
+#         lat = near_coffee_shop_dic['lat']
+#         lng = near_coffee_shop_dic['lng']        
+#         # set
+#         radius = 100
+#         keyword = 'coffee'
+#         nearby_coffee = get_nearby_places(lat, lng, radius, keyword)
 
-        if nearby_coffee:
-            nearest_coffee_shop = nearby_coffee[0]
-            photo_ref = nearest_coffee_shop['photos'][0]['photo_reference']
-            photo_width = nearest_coffee_shop['photos'][0]['width']
-            thumbnail_image_url = f"https://maps.googleapis.com/maps/api/place/photo?key={GOOGLE_API_KEY}&photoreference={photo_ref}&maxwidth={photo_width}"
-            nearest_coffee_details = get_place_details(nearest_coffee_shop['place_id'])
-            coffee_name = nearest_coffee_details['name']
-            coffee_rating = nearest_coffee_details['rating']
-            maps_url = f'https://www.google.com/maps/search/?api=1&query={lat},{lng}&query_place_id={nearest_coffee_shop["place_id"]}'
+#         if nearby_coffee:
+#             nearest_coffee_shop = nearby_coffee[0]
+#             photo_ref = nearest_coffee_shop['photos'][0]['photo_reference']
+#             photo_width = nearest_coffee_shop['photos'][0]['width']
+#             thumbnail_image_url = f"https://maps.googleapis.com/maps/api/place/photo?key={GOOGLE_API_KEY}&photoreference={photo_ref}&maxwidth={photo_width}"
+#             nearest_coffee_details = get_place_details(nearest_coffee_shop['place_id'])
+#             coffee_name = nearest_coffee_details['name']
+#             coffee_rating = nearest_coffee_details['rating']
+#             maps_url = f'https://www.google.com/maps/search/?api=1&query={lat},{lng}&query_place_id={nearest_coffee_shop["place_id"]}'
+        coffee_shop = nearest_coffee("新北市新莊區民樂街39號")
+        coffee_name = coffee_shop[0]
+        coffee_rating = coffee_shop[1]
+        maps_url = coffee_shop[2]
+        thumbnail_image_url = coffee_shop[3]
 #         coffee_name = "Cafefe Libero"
 #         coffee_rating = "4.2"
 #         thumbnail_image_url = "https://play-lh.googleusercontent.com/Kf8WTct65hFJxBUDm5E-EpYsiDoLQiGGbnuyP6HBNax43YShXti9THPon1YKB6zPYpA"
