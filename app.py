626250c444f9d47f67aa6fcd6fca048d67ea225d
tacochan0129 <119779506+tacochan0129@users.noreply.github.com>
2023-03-17 08:23:34 +0800

    附近店家debug12
    將輸出形式改成訊息

--- app.py
+++ app.py
@@ -209,7 +209,7 @@ def handle_message(event):
     # 附近店家功能
     if re.match('附近店家',message):
 #         line_bot_api.reply_message(event.reply_token, TextSendMessage('請傳送指定的位置喔~'))
-#     if event.message.type == "location:
+#     if event.message.type == 'location':
 #         address = event.message.address
 
         near_coffee_shop_location = "106台北市大安區永康街37巷28號"
@@ -221,33 +221,36 @@ def handle_message(event):
         keyword = 'coffee'
         nearby_coffee = get_nearby_places(lat, lng, radius, keyword)
 
-        nearest_coffee_shop = nearby_coffee[0]
-        photo_ref = nearest_coffee_shop['photos'][0]['photo_reference']
-        photo_width = nearest_coffee_shop['photos'][0]['width']
-        thumbnail_image_url = f"https://maps.googleapis.com/maps/api/place/photo?key={GOOGLE_API_KEY}&photoreference={photo_ref}&maxwidth={photo_width}"
-        nearest_coffee_details = get_place_details(nearest_coffee_shop['place_id'])
-        coffee_name = nearest_coffee_details['name']
-        coffee_rating = nearest_coffee_details['rating']
-        maps_url = f'https://www.google.com/maps/search/?api=1&query={lat},{lng}&query_place_id={nearest_coffee_shop["place_id"]}'
+        if nearby_coffee:
+            nearest_coffee_shop = nearby_coffee[0]
+            photo_ref = nearest_coffee_shop['photos'][0]['photo_reference']
+            photo_width = nearest_coffee_shop['photos'][0]['width']
+            thumbnail_image_url = f"https://maps.googleapis.com/maps/api/place/photo?key={GOOGLE_API_KEY}&photoreference={photo_ref}&maxwidth={photo_width}"
+            nearest_coffee_details = get_place_details(nearest_coffee_shop['place_id'])
+            coffee_name = nearest_coffee_details['name']
+            coffee_rating = nearest_coffee_details['rating']
+            maps_url = f'https://www.google.com/maps/search/?api=1&query={lat},{lng}&query_place_id={nearest_coffee_shop["place_id"]}'
 #         coffee_name = "Cafefe Libero"
 #         coffee_rating = "4.2"
 #         thumbnail_image_url = "https://play-lh.googleusercontent.com/Kf8WTct65hFJxBUDm5E-EpYsiDoLQiGGbnuyP6HBNax43YShXti9THPon1YKB6zPYpA"
 #         maps_url = "https://www.google.com.tw/maps/place/SECOND+FLOOR+CAFE+%E8%B2%B3%E6%A8%93%E4%BB%81%E6%84%9B/@25.0379115,121.5236378,15z/data=!3m1!5s0x3442a97ebf47ca7b:0x6fe70de6eeb4a6e4!4m6!3m5!1s0x3442a97ebf69e67b:0xf06276ea3de8b70!8m2!3d25.0379126!4d121.5323917!16s%2Fg%2F12hk8x73m"
-        buttons_template_message = TemplateSendMessage(
-        alt_text = '附近店家',
-        template=ButtonsTemplate(
-            thumbnail_image_url = thumbnail_image_url,
-            title = coffee_name,
-            text = "評分：" + coffee_rating,
-            actions = [
-                URIAction(
-                    label = '現在就過去吧！',
-                    uri = maps_url
-                )
-            ]
-        )
-    )
-        line_bot_api.reply_message(event.reply_token, buttons_template_message)        
+#         buttons_template_message = TemplateSendMessage(
+#         alt_text = '附近店家',
+#         template=ButtonsTemplate(
+#             thumbnail_image_url = thumbnail_image_url,
+#             title = coffee_name,
+#             text = "評分：" + coffee_rating,
+#             actions = [
+#                 URIAction(
+#                     label = '現在就過去吧！',
+#                     uri = maps_url
+#                 )
+#             ]
+#         )
+#     )
+#         line_bot_api.reply_message(event.reply_token, buttons_template_message)
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(coffee_name))
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(maps_url))
     else:
         line_bot_api.reply_message(event.reply_token, TextSendMessage('感謝您的使用❤️'))
 
