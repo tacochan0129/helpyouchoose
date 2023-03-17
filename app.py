5242e80323b8956bd9bf3bd7cd767affb253ed76
Lucas20030209 <96294647+Lucas20030209@users.noreply.github.com>
2023-03-18 02:12:43 +0800

    debug 30
    reply_message

--- app.py
+++ app.py
@@ -58,6 +58,8 @@ def callback():
 
     return 'OK'
 
+from linebot.models import TextSendMessage, LocationSendMessage, TemplateSendMessage, CarouselTemplate, CarouselColumn
+
 # function to get the address from the LocationMessage event
 def get_address(event):
     latitude = event.message.latitude
@@ -69,7 +71,6 @@ def get_address(event):
     else:
         return None
 
-from linebot.models import LocationMessage
 # handle location message event
 @handler.add(MessageEvent, message=LocationMessage)
 def handle_location_message(event):
@@ -93,32 +94,33 @@ def handle_location_message(event):
 #             coffee_name = nearest_coffee_details['name']
 #             coffee_rating = nearest_coffee_details['rating']
 #             maps_url = f'https://www.google.com/maps/search/?api=1&query={lat},{lng}&query_place_id={nearest_coffee_shop["place_id"]}'
-    coffee_shop = nearest_coffee(address)
-    coffee_name = coffee_shop[0]
-    coffee_rating = str(coffee_shop[1])
-    maps_url = coffee_shop[2]
-    thumbnail_url = coffee_shop[3]
+#     coffee_shop = nearest_coffee(address)
+#     coffee_name = coffee_shop[0]
+#     coffee_rating = str(coffee_shop[1])
+#     maps_url = coffee_shop[2]
+#     thumbnail_url = coffee_shop[3]
 #         coffee_name = "Cafefe Libero"
 #         coffee_rating = "4.2"
 #         thumbnail_image_url = "https://play-lh.googleusercontent.com/Kf8WTct65hFJxBUDm5E-EpYsiDoLQiGGbnuyP6HBNax43YShXti9THPon1YKB6zPYpA"
 #         maps_url = "https://www.google.com.tw/maps/place/SECOND+FLOOR+CAFE+%E8%B2%B3%E6%A8%93%E4%BB%81%E6%84%9B/@25.0379115,121.5236378,15z/data=!3m1!5s0x3442a97ebf47ca7b:0x6fe70de6eeb4a6e4!4m6!3m5!1s0x3442a97ebf69e67b:0xf06276ea3de8b70!8m2!3d25.0379126!4d121.5323917!16s%2Fg%2F12hk8x73m"
-    buttons_template_message = TemplateSendMessage(
-    alt_text = '附近店家',
-    template=ButtonsTemplate(
-        thumbnail_image_url = thumbnail_url,
-        title = coffee_name,
-        text = "評分：" + coffee_rating,
-        actions = [
-            URIAction(
-                label = '現在就過去吧！',
-                uri = maps_url
-            )
-        ]
-    )
-)
-    line_bot_api.reply_message(event.reply_token, buttons_template_message)
-#         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=address))
-#         line_bot_api.reply_message(event.reply_token, TextSendMessage(maps_url))
+#     buttons_template_message = TemplateSendMessage(
+#     alt_text = '附近店家',
+#     template=ButtonsTemplate(
+#         thumbnail_image_url = thumbnail_url,
+#         title = coffee_name,
+#         text = "評分：" + coffee_rating,
+#         actions = [
+#             URIAction(
+#                 label = '現在就過去吧！',
+#                 uri = maps_url
+#             )
+#         ]
+#     )
+# )
+    reply_message = TextSendMessage(text=address)
+#     line_bot_api.reply_message(event.reply_token, buttons_template_message)
+    line_bot_api.reply_message(event.reply_token, reply_message)
+#     line_bot_api.reply_message(event.reply_token, TextSendMessage(maps_url))
 
 
 #訊息傳遞區塊
